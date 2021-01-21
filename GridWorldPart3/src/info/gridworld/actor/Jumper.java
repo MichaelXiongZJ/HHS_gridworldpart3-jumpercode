cc23c5e722e809a779a50e2e78f427e050d4e5f6
Michael Xiong <MichaelThePanda@users.noreply.github.com>
2021-01-21 12:58:58 -0800

    update

--- GridWorldPart3/src/info/gridworld/actor/Jumper.java
+++ GridWorldPart3/src/info/gridworld/actor/Jumper.java
@@ -8,28 +8,48 @@ public class Jumper extends Actor{
 	
 	
 	public Jumper() {
-		
+		setColor(Color.RED);
 	}
 	
 	
 	public void turn() {
-		
+		setDirection(getDirection() + Location.HALF_RIGHT);
 	}
 	
 	
 	public void move() {
-		
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
 	}
 	
 	
 	public void act() {
-		
+		 if (canMove())
+	            move();
+	        else
+	            turn();
 	}
 	
 	public boolean canMove() {
 		
-		
-		return false;
+		Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
 	}
 	
 	
